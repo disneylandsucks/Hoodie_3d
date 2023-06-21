cd7f44240dc8c398b66762a9a2cb9fbb11663617
Arthur Dean <91149449+disneylandsucks@users.noreply.github.com>
2023-06-21 16:51:44 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -23,7 +23,7 @@ imgs[0]= loadImage('https://res.cloudinary.com/painkillerkid/image/upload/v16871
     
     imgs[7] = loadImage('https://res.cloudinary.com/painkillerkid/image/upload/v1687389824/Hoodie_Trial7_koxmwj.png')    
   imgs[8] = loadImage('https://res.cloudinary.com/painkillerkid/image/upload/v1687390557/Hoodie_Trial8_hpjcef.png')
-//     imgs[9] = loadImage(' https://res.cloudinary.com/painkillerkid/image/upload/v1665948981/oskiweddinground2_egydil.png')
+    imgs[9] = loadImage('https://res.cloudinary.com/painkillerkid/image/upload/v1687391454/Hoodie_Trial9_pgktbd.png')
 //       imgs[10] = loadImage(' https://res.cloudinary.com/painkillerkid/image/upload/v1665949784/toadpacshirt_3dthing_j0satg.png')
 
 //         imgs[11] = loadImage(' https://res.cloudinary.com/painkillerkid/image/upload/v1665957356/housedrawing_3dShirt_nf3yob.png')
