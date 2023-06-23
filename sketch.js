5e134344b015a3c033bdb85c41eb13580b1f168a
Arthur Dean <91149449+disneylandsucks@users.noreply.github.com>
2023-06-22 17:57:39 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -65,7 +65,7 @@ imgs[0]= loadImage('https://res.cloudinary.com/painkillerkid/image/upload/v16871
     
               imgs[30] = loadImage('https://res.cloudinary.com/painkillerkid/image/upload/v1687481374/Hoodie_Trial29_a33wjq.png')
 
-                  // imgs[31] = loadImage('https://res.cloudinary.com/painkillerkid/image/upload/v1687481374/Hoodie_Trial29_a33wjq.png')
+                  imgs[31] = loadImage('https://res.cloudinary.com/painkillerkid/image/upload/v1687481804/Hoodie_Trial31_k0zqq2.png')
 
 
                   // imgs[32] = loadImage('https://res.cloudinary.com/painkillerkid/image/upload/v1687481374/Hoodie_Trial29_a33wjq.png')
